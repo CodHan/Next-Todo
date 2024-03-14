ed35c07f9a942e37c24f58d943b030740cedc0a2
CodHan <qpscm662@naver.com>
2024-03-15 05:39:06 +0900

    about페이지,report페이지 기능 완료

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,24 @@
+import Image from 'next/image';
 import React from 'react';
 
-const AboutPage = () => {
-  return <div>AboutPage입니다.</div>;
+const AboutPage = async () => {
+  const req = await fetch('http://localhost:3000/api/company', {
+    method: 'GET',
+  });
+  const res = await req.json();
+  const data = res.data;
+  const { name, desctiption, image } = data;
+
+  return (
+    <div>
+      <div>회사명: {name}</div>
+      <div>설명: {desctiption}</div>
+      <div>회사 전경</div>
+      {/* <Image src={image} alt="회사이미지" /> */}
+    </div>
+  );
 };
 
 export default AboutPage;
+
+//이미지 불러오는거 오류
--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,7 +1,45 @@
+import { Todo } from '@/types/type';
 import React from 'react';
 
-const reportPage = () => {
-  return <div>reportPage</div>;
+const reportPage = async () => {
+  const req = await fetch('http://localhost:3000/api/todos', {
+    method: 'GET',
+    next: {
+      revalidate: 10,
+    },
+  });
+  const res = await req.json();
+  const data: Todo[] = res.todos;
+  const workingTodo: Todo[] = data.filter((item) => item.isDone === false);
+  const doneTodo: Todo[] = data.filter((item) => item.isDone === true);
+
+  return (
+    <div>
+      <div>현재 해야 할 todo는{workingTodo.length}개 입니다.</div>
+      <div>현재 완료 한 todo는{doneTodo.length}개 입니다.</div>
+    </div>
+  );
 };
 
 export default reportPage;
+
+// 'use client';
+// import React, { useEffect } from 'react';
+
+// const reportPage = () => {
+//   useEffect(() => {
+//     const data = async () => {
+//       const req = await fetch('http://localhost:3000/api/todos', {
+//         method: 'GET',
+//       });
+//       const res = await req.json();
+//       console.log(res.todos);
+//       const data = res.todos;
+//     };
+//     data();
+//   }, []);
+
+//   return <div>reportPage</div>;
+// };
+
+// export default reportPage;
